3a4731f4507e14371ee443bd578de7a5a56d4a69
lymin0109 <83511361+lymin0109@users.noreply.github.com>
2023-04-14 02:09:04 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -143,8 +143,8 @@ const goResult = () => {
   res_point.innerHTML = point + '%';
   pin.style.marginLeft = infoList[grade].mLeft;
   res_img.src = img_url;
-  res_img.alt = infoList[grade].name + infoList2[grade2-1].name + infoList3[grade3-1].name;
-  res_img.title = infoList[grade].name + infoList2[grade2-1].name + infoList3[grade3-1].name;;
+  res_img.alt = infoList[grade].name + infoList2[grade2-2].name + infoList3[grade3-2].name;
+  res_img.title = infoList[grade].name + infoList2[grade2-2].name + infoList3[grade3-2].name;;
   res_img_div.appendChild(res_img);
   animal.innerHTML = infoList[grade].name + "<br><br>" + infoList2[grade2-2].name + "<br><br>" + infoList3[grade3-2].name;
   desc.innerHTML = infoList[grade].desc + "<br><br>" + infoList2[grade2-2].desc + "<br><br>" + infoList3[grade3-2].desc;
