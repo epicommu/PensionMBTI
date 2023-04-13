2fe34491b3b49a77c0be617846e2b8d56e343ccd
lymin0109 <83511361+lymin0109@users.noreply.github.com>
2023-04-13 20:11:39 +0900

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,110 @@
+const qnaList = [
+  {
+    q: '1. 둘 중 하나만 투자한다면?',
+    a: [
+      { answer: 'a. 금', score: 0 },
+      { answer: 'b. 도지코인', score: 5 }
+    ]
+  },
+  {
+    q: '2. 둘 중 하나만 투자한다면?',
+    a: [
+      { answer: 'a. 미국 국채', score: 0 },
+      { answer: 'b. 테슬라', score: 5 }
+    ]
+  },
+  {
+    q: '3. 둘 중 하나만 투자한다면?',
+    a: [
+      { answer: 'a. 나스닥', score: 0 },
+      { answer: 'b. 비트코인', score: 5 }
+    ]
+  },
+  {
+    q: '4. 둘 중 하나만 투자한다면?',
+    a: [
+      { answer: 'a. 금', score: 0 },
+      { answer: 'b. 유가', score: 5 }
+    ]
+  },
+  {
+    q: '5. 둘 중 하나만 투자한다면?',
+    a: [
+      { answer: 'a. 삼성전자', score: 0 },
+      { answer: 'b. 나스닥', score: 5 }
+    ]
+  },
+  {
+    q: '6. 둘 중 하나만 투자한다면?',
+    a: [
+      { answer: 'a. 코스피', score: 0 },
+      { answer: 'b. 삼성전자', score: 5 }
+    ]
+  },
+  {
+    q: '7. 채권 투자시 둘 중 하나를 고른다면?<br><br><img src="img/image001.jpg" alt="image001"><br>',
+    a: [
+      { answer: 'a', score: 0, score2: 0 },
+      { answer: 'b', score: 5, score2: 1 }
+    ]
+  },
+  {
+    q: '8. 채권 투자시 둘 중 하나를 고른다면?<br><br><img src="img/image002.jpg" alt="image002"><br>',
+    a: [
+      { answer: 'a', score: 0, score2: 0 },
+      { answer: 'b', score: 5, score2: 1 }
+    ]
+  },
+  {
+    q: '9. 채권 투자시 둘 중 하나를 고른다면?<br><br><img src="img/image003.jpg" alt="image003"><br>',
+    a: [
+      { answer: 'a', score: 0, score2: 0 },
+      { answer: 'b', score: 5, score2: 1 }
+    ]
+  },
+  {
+    q: '10. 채권 투자시 둘 중 하나를 고른다면?<br><br><img src="img/image004.jpg" alt="image004"><br>',
+    a: [
+      { answer: 'a', score: 0, score2: 0 },
+      { answer: 'b', score: 5, score2: 1 }
+    ]
+  }
+]
+
+const infoList = [
+  {
+    from: 0,
+    to: 9,
+    mLeft: '10%',
+    name: '채권 몰빵형',
+    desc: '주식 100% 몰빵 포트폴리오'
+  },
+  {
+    from: 10,
+    to: 24,
+    mLeft: '30%',
+    name: '채권혼합형',
+    desc: '60:40(채권:주식) 포트폴리오'
+  },
+  {
+    from: 25,
+    to: 30,
+    mLeft: '50%',
+    name: '뭐든지 반반형',
+    desc: '50:50(채권:주식) 포트폴리오'
+  },
+  {
+    from: 31,
+    to: 44,
+    mLeft: '70%',
+    name: '주식혼합형',
+    desc: '40:60(채권:주식) 포트폴리오'
+  },
+  {
+    from: 45,
+    to: 50,
+    mLeft: '90%',
+    name: '주식 몰빵형',
+    desc: '주식 100% 몰빵 포트폴리오'
+  }
+]
