d8c259b6ab9f5c2cf200d8bb9a84bae7c09a4cb3
lymin0109 <83511361+lymin0109@users.noreply.github.com>
2023-04-18 20:32:57 +0900

    Update spread_sheet.js

--- js/spread_sheet.js
+++ js/spread_sheet.js
@@ -1,19 +1,27 @@
 // 구글 API 클라이언트 라이브러리 로드
-gapi.load('client', function() {
-  // 구글 API 클라이언트 초기화
-  gapi.client.init({
+gapi.load('client:auth2', initClient);
+
+// API 클라이언트 초기화
+function initClient() {
+  // 초기화 설정
+  var initOptions = {
     apiKey: 'AIzaSyDjEbE4vsbThMNDp_Pb_42k8Tewi0QEShI',
-    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     clientId: '223843052834-okubm0hl7i4pie5cibq4u100oqkcu3o2.apps.googleusercontent.com',
-    scope: 'https://www.googleapis.com/auth/spreadsheets'
-  }).then(function() {
-    // 클라이언트 초기화 성공 시 실행할 함수
+    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    scope: 'https://www.googleapis.com/auth/spreadsheets',
+  };
+
+  // 클라이언트 초기화
+  gapi.client.init(initOptions).then(function() {
+    // 인증 상태를 확인하고, 인증되지 않았으면 로그인
+    if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
+      gapi.auth2.getAuthInstance().signIn();
+    }
     saveDataToSheet();
   }, function(error) {
-    // 클라이언트 초기화 실패 시 실행할 함수
     console.log(error);
   });
-});
+}
 
 // 데이터 저장 함수
 function saveDataToSheet() {
@@ -43,7 +51,9 @@ function saveDataToSheet() {
     valueInputOption: 'USER_ENTERED',
     resource: valueRange
   });
-  request.execute(function(response) {
+  request.then(function(response) {
     console.log(response);
+  }, function(error) {
+    console.log(error);
   });
 }
