6a7da5c9c0b59349047b515edec1fe4781ca7f0e
lymin0109 <83511361+lymin0109@users.noreply.github.com>
2023-04-19 01:41:15 +0900

    Update spread_sheet.js

--- js/spread_sheet.js
+++ js/spread_sheet.js
@@ -1,20 +1,18 @@
 // 1. 구글 API 클라이언트 로드
-gapi.load('client:auth2', initClient);
+gapi.load('client', initClient);
 
 // 2. API 클라이언트 초기화
 function initClient() {
-gapi.client.init({
-apiKey: 'AIzaSyDjEbE4vsbThMNDp_Pb_42k8Tewi0QEShI',
-discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-clientId: '223843052834-okubm0hl7i4pie5cibq4u100oqkcu3o2.apps.googleusercontent.com',
-scope: 'https://www.googleapis.com/auth/spreadsheets'
-}).then(function () {
-// 클라이언트 초기화 성공 시 실행할 함수
-saveDataToSheet();
-}, function (error) {
-// 클라이언트 초기화 실패 시 실행할 함수
-console.log(error);
-});
+  gapi.client.init({
+    apiKey: 'AIzaSyDjEbE4vsbThMNDp_Pb_42k8Tewi0QEShI',
+    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  }).then(function () {
+    // 클라이언트 초기화 성공 시 실행할 함수
+    saveDataToSheet();
+  }, function (error) {
+    // 클라이언트 초기화 실패 시 실행할 함수
+    console.log(error);
+  });
 }
 
 // 데이터 저장 함수
@@ -37,30 +35,18 @@ function saveDataToSheet() {
     var range = sheetName + '!A' + nextRow + ':C' + nextRow; // 다음 행에 추가하도록 range 값 수정
 
     // 구글 API를 사용하여 데이터 저장
-    appendValues(spreadsheetId, range, 'USER_ENTERED', data, function(response) {
-      console.log(response);
-    });
-  });
-}
-
-function appendValues(spreadsheetId, range, valueInputOption, _values, callback) {
-  let values = _values;
-  const body = {
-    values: values,
-  };
-  try {
     gapi.client.sheets.spreadsheets.values.append({
       spreadsheetId: spreadsheetId,
       range: range,
-      valueInputOption: valueInputOption,
-      resource: body,
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: data
+      }
     }).then((response) => {
       const result = response.result;
       console.log(`${result.updates.updatedCells} cells appended.`);
-      if (callback) callback(response);
+    }, function (error) {
+      console.log(error);
     });
-  } catch (err) {
-    document.getElementById('content').innerText = err.message;
-    return;
-  }
+  });
 }
