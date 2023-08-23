795bf115e37359f9372bb5d30d735a9af45e3cb1
GGYYUUPark <141404606+GGYYUUPark@users.noreply.github.com>
2023-08-23 18:28:46 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,6 +9,38 @@ const ENDPOINT = 16;
 const select = [];
 let qIdx = -1;
 
+
+var shopLink = "a";
+var shopLink2 = "b";
+
+if(hyungu.innerHTML == "당신의 고유 MBTI는... INTJ1000"){
+  shopLink = "https://etfpi.cafe24.com/surl/O/1961";
+}  
+if(hyungu.innerHTML == "당신의 고유 MBTI는... INTJ1001"){
+  shopLink = "https://etfpi.cafe24.com/surl/O/1962";
+}  
+
+const hyunguLink1 = document.querySelector(".hyungyuLink1");
+hyunguLink1.setAttribute("href", shopLink)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const goTo = (dest) => {
   let elem;
   let elemTop;
